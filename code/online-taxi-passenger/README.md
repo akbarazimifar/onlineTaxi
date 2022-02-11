54a37d0d6421b6d21b0e4dbd81be0f3c074f790f
dunk <wy471x@qq.com>
2022-02-11 18:02:48 +0800

    finishing...

---
+++ code/online-taxi-passenger/README.md
@@ -0,0 +1,4 @@
+# 工程简介
+online-taxi-passenger 乘客服务
+# 延伸阅读
+
---
+++ code/online-taxi-passenger/src/test/java/cn/dunk/onlinetaxi/passenger/OnlineTaxiPassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dunk.onlinetaxi.passenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTaxiPassengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
